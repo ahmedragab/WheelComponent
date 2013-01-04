ee8b58f7b4d8594f05bbb8178f36329d2777736d
Ahmed <ahmed.ragab@me.com>
2013-01-04 18:35:42 +0200

    Initial Commit

---
+++ WheelComponent/GNAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GNAppDelegate.h
+//  WheelComponent
+//
+//  Created by Ahmed Ragab on 1/4/13.
+//  Copyright (c) 2013 AhmedRagab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GNViewController;
+
+@interface GNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) GNViewController *viewController;
+
+@end
---
+++ WheelComponent/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WheelComponent
+//
+//  Created by Ahmed Ragab on 1/4/13.
+//  Copyright (c) 2013 AhmedRagab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GNAppDelegate class]));
+    }
+}
